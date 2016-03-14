acd5728f36396d4f1e1e0d594515832c0ac4dbde
taitanxiami <taitanxiami@gmail.com>
2016-03-14 10:48:39 +0800

    Initial Commit

---
+++ DynamicCellH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicCellH
+//
+//  Created by taitanxiami on 16/3/14.
+//  Copyright © 2016年 taitanxiami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
